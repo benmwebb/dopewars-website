05d5b9e1cbe626f202f09df3c54e0f8b45664ad4
Ben Webb <ben@salilab.org>
2020-10-30 07:57:27 +0000

    Disable cache for metaserver

--- metaserver.php
+++ metaserver.php
@@ -8,6 +8,11 @@
     if (!$dbhand) {
       FatalError("Could not connect to dopewars database server");
     }
+
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
     if ($_REQUEST['output'] == 'text') {
       header("Content-type: text/plain");
       $textoutput=TRUE;
